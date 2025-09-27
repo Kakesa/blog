473bf8403a570acbab2d5bedd238e6bed63734fa
Espoir Kakesa <120624185+Kakesa@users.noreply.github.com>
2025-09-27 03:32:30 +0100

    fix : add button login on the home page

--- app/controllers/auth_controller.ts
+++ app/controllers/auth_controller.ts
@@ -5,10 +5,10 @@ import { loginValidator } from '#validators/login'
 
 export default class AuthController {
   public async register({ request, response, auth }: HttpContext) {
+    // Validation avec Vine
     const payload = await request.validateUsing(registerValidator)
 
     const user = await User.create(payload)
-
     await auth.use('web').login(user)
 
     return response.redirect('/')
@@ -18,14 +18,11 @@ export default class AuthController {
     const { email, password } = await request.validateUsing(loginValidator)
 
     try {
-      // Vérifie les identifiants
       const user = await User.verifyCredentials(email, password)
-
-      // Connecte l’utilisateur après vérification
       await auth.use('web').login(user)
-
       return response.redirect('/')
-    } catch {
+    } catch (error) {
+      console.error(error)
       return response.badRequest('Email ou mot de passe incorrect')
     }
   }
